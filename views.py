8235fb04368688ff9bf9f3917bbd4c7c45ef4144
Krzysztof Witek <kwitek@kw-net.com>
2021-02-17 09:01:23 +0100

    initial commit
    Signed-off-by: Krzysztof Witek <kwitek@kw-net.com>

---
+++ views.py
@@ -0,0 +1,76 @@
+from django.shortcuts import render
+from django import http
+from django.contrib.auth.decorators import login_required
+from os import listdir
+from os.path import isdir, isfile, join
+import json
+
+reports_path = '/management/desktop-updates/reports'
+
+@login_required
+def status_view(request):
+    if not request.user.is_superuser:
+        return http.HttpResponseForbidden()
+
+    #hosts = [d for d in listdir(reports_path) if isdir(join(reports_path, d))]
+    hosts = []
+    cpt_total = 0
+    for d in listdir(reports_path):
+        if not isdir(join(reports_path, d)):
+            continue
+        cpt_total += 1
+
+        try:
+            with open(join(reports_path, d, 'report.json')) as json_file:
+                status = json.load(json_file)
+        except:
+            status = {}
+        try:
+            with open(join(reports_path, d, 'updates_list')) as updates_file:
+                for last_update in updates_file:
+                    pass
+        except:
+            last_update = '- 0'
+        try:
+            with open(join(reports_path, '..', 'last_check')) as last_check_file:
+                last_check = last_check_file.read()
+        except:
+            last_check = 'Never'
+
+        try:
+            with open(join(reports_path, '..', 'count')) as count_file:
+                counters = count_file.read()
+        except:
+            cpt_connected = 'N/A'
+            cpt_failed = 'N/A'
+        counters = counters.split()
+        if len(counters) == 2:
+            cpt_connected = counters[0]
+            cpt_failed = counters[1]
+        else:
+            cpt_connected = 0
+            cpt_failed = 0            
+            
+        if isfile(join(reports_path, d, 'connect_failed')):
+            status['connect_failed'] = 'Connection failed'
+
+        _last_update = last_update.split()
+        if len(_last_update) == 2:
+            last_update = _last_update[0]
+            last_update_rc = _last_update[1]
+
+        status['ip'] = d
+        status['last_update'] = last_update
+        status['last_update_rc'] = last_update_rc
+        hosts.append(status)
+
+    context = {
+        'title': 'Hosts',
+        'hosts': hosts,
+        'last_check': last_check,
+        'cpt_total' : cpt_total,
+        'cpt_connected' : cpt_connected,
+        'cpt_failed' : cpt_failed,
+    }
+    return render(request, 'host_monitor/list.html', context)
+
